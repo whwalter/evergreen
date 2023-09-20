35d2424dc4cd2cad9876448cc9786d53265a7916
April White <april.white@mongodb.com>
2023-09-20 10:32:17 -0700

    Point to the Dockerfile

--- build/production-build.sh
+++ build/production-build.sh
@@ -3,6 +3,7 @@
 GIT_HASH=$(git rev-parse --short HEAD)
 
 docker buildx build --platform=linux/arm64 \
+    -f build/Dockerfile \
     --build-arg GIT_HASH=${GIT_HASH} \
     --build-arg GOOS="linux" \
     --build-arg GOARCH="arm64" \
